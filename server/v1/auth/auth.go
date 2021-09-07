71c181e250d08da3509684032a00691b1f9e6f8d
KoGyeongtae <kokt0203@naver.com>
2021-09-07 22:39:22 +0900

    EDIT

--- server/v1/auth/auth.go
+++ server/v1/auth/auth.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 
 	"github.com/Backend-GAuth-server/dto"
+	"github.com/Backend-GAuth-server/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Login(c *fiber.Ctx) error {
 	var req dto.LoginReq
-	c.Body()
+	utils.ByteToObj(c.Body(), &req)
 	fmt.Println(req)
-	return c.JSON(req)
+	utils.MarshalAndRes(200, req, c)
+	return nil
 }
 
 func Signup(c *fiber.Ctx) error {
