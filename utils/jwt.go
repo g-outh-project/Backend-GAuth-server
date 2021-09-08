31403105d323ddd52b84f84e219de24feae661d4
KoGyeongtae <kokt0203@naver.com>
2021-09-08 18:57:59 +0900

    ADD - jwt utils

---
+++ utils/jwt.go
@@ -0,0 +1,93 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+)
+
+// test jwtSignKey
+var testSignKey = []byte("TestForFasthttpWithJWT")
+
+// Credential type
+type userCredential struct {
+	Email     string `json:"email"`
+	IsStudent bool   `json:"isStudent"`
+	jwt.StandardClaims
+}
+
+// GetToken function
+func GetTokenString(c *fiber.Ctx) ([]byte, error) {
+	// Get token from request token
+	jwt := c.Request().Header.Header()
+
+	// Token length validation
+	if len(jwt) == 0 {
+		c.SendStatus(401)
+		return nil, errors.New("Token cannot found")
+	}
+
+	// Return token with type []byte
+	return []byte(jwt), nil
+}
+
+// Generate accessToken
+func AccessToken(email string, isStudent bool) string {
+	// Generate Token object
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS512, &userCredential{
+		Email:     email,
+		IsStudent: isStudent,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(10 * time.Minute).Unix(), // 10 Mins
+		},
+	})
+
+	// jwt Key
+	jwtSignKey := []byte(GetSecretKey())
+
+	// Sign token
+	access, err := accessToken.SignedString(jwtSignKey)
+	HandleErr(err)
+
+	return access
+}
+
+// Generate refreshToken
+func RefreshToken(email string, isStudent bool) string {
+	// Generate Token object
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS512, &userCredential{
+		Email:     email,
+		IsStudent: isStudent,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(336 * time.Hour).Unix(), // 14Days
+		},
+	})
+
+	// jwt Key
+	jwtSignKey := []byte(GetSecretKey())
+
+	// Sign token
+	refresh, err := refreshToken.SignedString(jwtSignKey)
+	HandleErr(err)
+
+	return refresh
+}
+
+// Validate token
+func ValidateToken(requestToken string) (*jwt.Token, *userCredential, error) {
+	// Generate Credential object
+	user := &userCredential{}
+
+	// jwt Key
+	jwtSignKey := []byte(GetSecretKey())
+
+	// Parse token and validate
+	token, err := jwt.ParseWithClaims(requestToken, user, func(token *jwt.Token) (interface{}, error) {
+		return jwtSignKey, nil
+	})
+	HandleErr(err)
+
+	return token, user, err
+}
