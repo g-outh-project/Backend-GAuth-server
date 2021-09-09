e5fc9f901ef045cec27c981daff60a910c2c39cb
KoGyeongtae <kokt0203@naver.com>
2021-09-09 10:51:48 +0900

    EDIT - dto

--- dto/login.d.go
+++ dto/login.d.go
@@ -1,5 +1,10 @@
 package dto
 
+type Jwt struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
 type LoginReq struct {
 	Id       string `json:"id"`
 	Password string `json:"password"`
