c440fde0ae1159469cc1953480ac9e2322a42f8f
KoGyeongtae <kokt0203@naver.com>
2021-09-13 19:05:59 +0900

    EDIT

--- dto/login.d.go
+++ dto/login.d.go
@@ -1,8 +1,22 @@
 package dto
 
+import "time"
+
 type Jwt struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
+}
+
+type JWTSource struct {
+	Uid               string    `json:"uid"`
+	Id                string    `json:"id"`
+	Email             string    `json:"email"`
+	Name              string    `json:"name"`
+	School            string    `json:"school"`
+	Birth             string    `json:"birth"`
+	Nickname          string    `json:"nickname"`
+	HashedAccessToken string    `json:"hashedAccessToken"`
+	CreatedAt         time.Time `json:"createdAt"`
 }
 
 type LoginReq struct {
