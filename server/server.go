705dd5949ff6ad1db10476f6d0e7039589c995a3
KoGyeongtae <kokt0203@naver.com>
2021-09-13 20:15:51 +0900

    EDIT - something

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"log"
 
+	"github.com/Backend-GAuth-server/db"
 	"github.com/Backend-GAuth-server/server/middleware"
 	v1 "github.com/Backend-GAuth-server/server/v1"
 	auth "github.com/Backend-GAuth-server/server/v1/auth"
@@ -13,14 +14,22 @@ import (
 )
 
 func Start() {
+	// Basic Setting of server
 	app := fiber.New()
 	file := utils.OpenLogger()
 
+	// Start and connect to DB
+	db.Start()
+
+	// Release resource
+	defer db.CloseDB()
 	defer file.Close()
 
+	// Middleware setting
 	app.Use(limiter.New(utils.Limiter()))
 	app.Use(logger.New(utils.Logger(file)))
 
+	// Routing
 	v1Router := app.Group("/api", middleware.JSONMiddleware)
 	v1Router.Get("/life", v1.Life)
 
--- server/v1/auth/auth.go
+++ server/v1/auth/auth.go
@@ -1,9 +1,10 @@
 package auth
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/Backend-GAuth-server/dto"
+	"github.com/Backend-GAuth-server/method"
 	"github.com/Backend-GAuth-server/utils"
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,17 +13,43 @@ func Login(c *fiber.Ctx) error {
 	var req dto.LoginReq
 	var res dto.LoginRes
 
-	utils.ByteToObj(c.Body(), &req)
-	res.AccessToken = utils.AccessToken(req.Id, req.Password)
-	res.RefreshToken = utils.RefreshToken(req.Id, req.Password)
+	userData := dto.JWTSource{
+		Uid:               "123",
+		Id:                "123",
+		Email:             "123",
+		Name:              "123",
+		School:            "123",
+		Birth:             "123",
+		Nickname:          "123",
+		HashedAccessToken: "",
+		CreatedAt:         time.Now().UTC(),
+	}
+
+	err := c.BodyParser(&req)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	res.AccessToken = utils.AccessToken(userData)
+	userData.HashedAccessToken = utils.Hash(res.AccessToken)
+	res.RefreshToken = utils.RefreshToken(userData)
 	utils.MarshalAndRes(200, res, c)
 	return nil
 }
 
 func Signup(c *fiber.Ctx) error {
 	var req dto.SignupReq
-	utils.ByteToObj(c.Body(), &req)
-	fmt.Println(req)
+
+	err := c.BodyParser(&req)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	method.InsertUser(req)
+
 	c.SendStatus(201)
 	return nil
 }
